7d9f824343bb4578f8b55bbdac57bde627226cb5
Animesh <animeshiit0806@gmail.com>
2025-03-11 11:19:49 +0530

    initial feature

---
+++ expense_tracker/lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,44 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge, 
+            ),
+            const SizedBox(height: 4), 
+            Text(
+              '\$${expense.amount.toStringAsFixed(2)}',
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
+            ),
+            const SizedBox(height: 8), 
+            Row(
+              children: [
+                Icon(categoryIcons[expense.category] as IconData?),
+                const SizedBox(width: 8),
+                Text(
+                  DateFormat.yMMMd().format(expense.date), 
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
